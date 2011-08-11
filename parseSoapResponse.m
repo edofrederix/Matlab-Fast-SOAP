ff75f0a5c64179af98241def9be5d73bcf6106ec
Edo Frederix <edofrederix@gmail.com>
2011-08-10 23:26:53 -0400

    Major changes in parseSoapResponse
    Data found with the footprint method will be returned in a char or double-precision vector. This makes the data more accessible. Also, more flexibility has been built in for complex XML structures.

--- parseSoapResponse.m
+++ parseSoapResponse.m
@@ -33,7 +33,7 @@
 
 function s = parseSoapResponse(response)
 
-    matches = regexpi(response, '<soap:Body>(?<content>.*)</soap:Body>', 'names');
+    matches = regexpi(response, '<soap:Body[^>]*>.*<(?<method>[a-z0-9]*)response[^>]*>(?<content>.*)</(?:[a-z0-9]*)response>.*</soap:Body>', 'names');
   
     s = fetchXML(matches.content);
     
@@ -41,26 +41,38 @@ end
 
 %==========================================================================
 function s = fetchXML(xml)
-    tic
-    [match matches] = regexpi(xml, '<(?<tag>[a-z0-9]*)[^>]*>(?<val>.*?)<[/]\1>', 'match', 'names');
-    toc
+
+    regex = '<(?<tag>[a-z0-9]*)[^>]*>(?<val>.*?)<[/]\1>';
+    [match matches] = regexpi(xml, regex, 'match', 'names');
     
     if isempty(matches)
         s = xml;
     else 
+        % Check tags
         s = struct();
         if numel(matches) == 1
             s.(matches.tag) = fetchXML(matches.val); 
         else
-            if footprint(match{1}) == footprint(match{2})
-                s.(matches(1).tag) = regexpi(xml, footprint(match{1}), 'names');
+            % Check footprint
+            fp1 = footprint(match{1});
+            fp2 = footprint(match{1});
+            if strcmp(fp1, fp2)
+                s.(matches(1).tag) = swapStruct(regexpi(xml, fp1, 'names'));
+                
+                % Check for remainder
+                xml = regexprep(xml, fp1, '', 'ignorecase');
+                if ~isempty(xml)
+                    s = mergeStruct(s, fetchXML(xml));
+                end 
+
             else
-                for i=1:numel(matches)        
-                    s(i).(matches(i).tag) = fetchXML(matches(i).val);   
+                for i=1:numel(matches)
+                    s.(matches(i).tag) = fetchXML(matches(i).val);
                 end
             end
-        end
+        end       
     end
+    
 end
 
 %==========================================================================
@@ -77,6 +89,43 @@ function fp = footprint(xml)
                 new = sprintf('(?<%s>.*?)', tok{i}{1});
             end
             fp = sprintf('%s<%s>%s</%s>', fp, tok{i}{1}, new, tok{i}{1});
+        end
+    end
+end
+
+%==========================================================================
+function s1 = mergeStruct(s1, s2)
+    if numel(s1) ~= numel(s2)
+        error('Arguments are not consistent in structure field number.');
+    end
+    
+    % Update s1 with s2
+    keys = fieldnames(s2);
+    for i = 1:numel(keys)
+        key = keys{i};
+        for j = 1:numel(s1)
+            s1(j).(key) = s2(j).(key);
+        end
+    end
+end
+
+%==========================================================================
+function s2 = swapStruct(s1)
+
+    keys = fieldnames(s1);
+    cl = cell(numel(keys), numel(s1));
+    for i = 1:numel(s1)
+        cl(:,i) = struct2cell(s1(i));
+    end
+    
+    s2 = struct();
+    for i = 1:numel(keys)
+        if strcmp(str2double(cl{1,1}), 'NaN')
+            s2.(keys{i}) = cl(i,:);
+        else
+            % Assuming numbers
+            c = char(cl(i,:));
+            s2.(keys{i}) = sscanf(c', '%f');
         end
     end
 end
